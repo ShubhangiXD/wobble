3d4e5e22bc4a5857974972805efd6bee558d7896
Renyxx <d.shubhangi1510@gmail.com>
2024-08-05 01:21:29 +0530

    Initial commit

---
+++ src/Overlay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { a } from '@react-spring/web'
+
+export default function Overlay({ fill }) {
+  // Just a Figma export, the fill is animated
+  return <div className="overlay"></div>
+}
